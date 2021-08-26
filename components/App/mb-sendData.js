ab39071e0d9b06b6cad1c4d036241ffe1b27b8a9
alexestudio86 <80129697+alexestudio86@users.noreply.github.com>
2021-08-26 13:19:46 -0500

    Update mb-sendData.js

--- components/App/mb-sendData.js
+++ components/App/mb-sendData.js
@@ -3,7 +3,7 @@
 // Send data
 const sendData = () => {
   // Phone number
-  whatsappNumber = '+5215510698215';
+  whatsappNumber = '+5217222478451';
   // Intro
   introText = '%5F'+encodeURIComponent('Hola, me gustaría ordenar')+'%5F%0A%0A';
   // Body
