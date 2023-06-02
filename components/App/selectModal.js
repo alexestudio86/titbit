d8a5b4315a3fa740dd857538ff89abd5d579d86a
alexestudio86 <80129697+alexestudio86@users.noreply.github.com>
2023-06-02 11:52:41 -0600

    Create selectModal.js

---
+++ components/App/selectModal.js
@@ -0,0 +1,28 @@
+// CREATE WELCOME WINDOW
+
+const createWelcome = () => {
+
+  modalGeneral();
+  const modal = document.getElementById('modal');
+  const fragment = document.createDocumentFragment();
+  const welcomeTemplate = document.getElementById('welcomeTemplate').content;
+  const clone = welcomeTemplate.cloneNode(true);
+
+  // Insert Fragment
+  fragment.appendChild(clone);
+
+  // Insert in modal
+  modal.appendChild(fragment);
+
+  // Add evento to close modal
+  modal.querySelector('button').addEventListener('click', () => {
+    modalGeneral();
+    localStorage.advice = 'true';
+  });
+
+}
+
+// Inicialice
+if( localStorage.advice === undefined ){
+  createWelcome();
+}
