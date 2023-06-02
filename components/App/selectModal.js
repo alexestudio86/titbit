03aa238bc0930bd38862383abb6f87d36511f956
alexestudio86 <80129697+alexestudio86@users.noreply.github.com>
2023-06-02 13:22:27 -0600

    Update selectModal.js

--- components/App/selectModal.js
+++ components/App/selectModal.js
@@ -8,21 +8,25 @@ const createWelcome = () => {
   const selectTemplate = document.getElementById('selectTemplate').content;
   const clone = selectTemplate.cloneNode(true);
 
+  // Add black style
+  modal.classList.add("w3-black");
+  
   // Insert Fragment
   fragment.appendChild(clone);
 
   // Insert in modal
   modal.appendChild(fragment);
 
-  // Add evento to close modal
+  // Add event to close modal
   modal.querySelector('button').addEventListener('click', () => {
+    modal.classList.remove("w3-black");
     modalGeneral();
-    sessionStorage.selectMenu = 'true';
+    //sessionStorage.selectMenu = 'true';
   });
 
 }
 
 // Inicialice
-if( sessionStorage.selectMenu === undefined ){
+//if( sessionStorage.selectMenu === undefined ){
   createWelcome();
-}
+//}
